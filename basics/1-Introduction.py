b68d809a2eb8fdbf677690148bee9b93bada9b22
Your Name <your-email@example.com>
2026-08-21 21:46:34 +0530

    Updated intro file

--- basics/1-Introduction.py
+++ basics/1-Introduction.py
@@ -1,16 +1,19 @@
 # Python is a high level language
 # Python is a interpreter
 
-print("HELLO WORLD")
+print("Hello World") # Prints this text on the terminal 
+
 print("THARUN")
-print("John")
-print("Dhanush")
+print("John is a Boy.")
+print("Dhanush is my Brother.")
+
 x = 5
 y = 6
-print(x+y)
-print(y-x)
-print(x*y)
+print(x+y) # Addition
+print(y-x) # Subtraction
+print(x*y) # Multiplication
+
 a = 10
 b = 2
-print(a**b)
-print(pow(a, b))
+print(a**b) # a power of b
+print(pow(a, b)) # same as above but using pow function
